11c871614afdb3c8d6bbecff84d73def202eafa8
pascal-thomas <pthomas@adobe.com>
2018-04-16 17:53:20 +0200

    Update SimpleServlet.java

--- code/TL19/core/src/main/java/adobesummit/core/servlets/SimpleServlet.java
+++ code/TL19/core/src/main/java/adobesummit/core/servlets/SimpleServlet.java
@@ -39,7 +39,7 @@ import java.io.IOException;
            property={
                    Constants.SERVICE_DESCRIPTION + "=Simple Demo Servlet",
                    "sling.servlet.methods=" + HttpConstants.METHOD_GET,
-                   "sling.servlet.resourceTypes="+ "L729/components/structure/page",
+                   "sling.servlet.resourceTypes="+ "TL19/components/structure/page",
                    "sling.servlet.extensions=" + "txt"
            })
 public class SimpleServlet extends SlingSafeMethodsServlet {
